2d5e9d07b22737b06974efc01150ab92a3a1ce65
Luigi Sgro <lsgrodev@gmail.com>
2020-02-12 22:00:44 +0100

    Simplified configuration with dummy auth and self-signed certificate

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -16,21 +16,7 @@ c.JupyterHub.admin_access = True
 c.Spawner.default_url = '/lab'
 
 ## Authenticator
-from jhub_cas_authenticator.cas_auth import CASAuthenticator
-c.JupyterHub.authenticator_class = CASAuthenticator
-
-# The CAS URLs to redirect (un)authenticated users to.
-c.CASAuthenticator.cas_login_url = 'https://cas.uvsq.fr/login'
-c.CASLocalAuthenticator.cas_logout_url = 'https://cas.uvsq/logout'
-
-# The CAS endpoint for validating service tickets.
-c.CASAuthenticator.cas_service_validate_url = 'https://cas.uvsq.fr/serviceValidate'
-
-# The service URL the CAS server will redirect the browser back to on successful authentication.
-c.CASAuthenticator.cas_service_url = 'https://%s/hub/login' % os.environ['HOST']
-
-c.Authenticator.admin_users = { 'lucadefe' }
-
+c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
 
 ## Docker spawner
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
@@ -44,17 +30,3 @@ c.JupyterHub.hub_ip = os.environ['HUB_IP']
 notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
 c.DockerSpawner.notebook_dir = notebook_dir
 c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
-
-# Other stuff
-c.Spawner.cpu_limit = 1
-c.Spawner.mem_limit = '10G'
-
-
-## Services
-c.JupyterHub.services = [
-    {
-        'name': 'cull_idle',
-        'admin': True,
-        'command': 'python /srv/jupyterhub/cull_idle_servers.py --timeout=3600'.split(),
-    },
-]
